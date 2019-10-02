25a74fddc6a9b776d62dc0962b46b485601a05b1
soskill71 <kartikjodhpur@gmail.com>
2019-10-02 18:53:25 +0530

    first commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kartik Kumar on 24/08/19.
+//  Copyright © 2019 Kartik Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+#My first iOS App
+
+I Am Rich
